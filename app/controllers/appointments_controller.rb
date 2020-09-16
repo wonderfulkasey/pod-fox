288d1ff7527fceeb047ecdbde6a965411cddc172
Jonathan East <jonathan_east@ymail.com>
2020-09-16 16:34:52 -0400

    updated index and added show routes with views and controllers

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -4,4 +4,26 @@ class AppointmentsController < ApplicationController
         @apps = Appointment.all
     end
 
+    def new
+      @app = Appointment.new
+    end
+
+    def show
+      @app = Appointment.find(params[:id])
+    end
+
+
+    def create
+      @app = Appointment.new(app_params)
+      @app.save
+      redirect_to appointment_path(@app)
+    end
+
+
+    private
+
+    def app_params
+      params.require(:appointment).permit(:time, :date)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/appointments', to: 'appointments#index'
+  get '/appointments/new', to: 'appointments#new'
+  get '/appointments/:id', to: 'appointments#show', as: "appointment"
 
 end
