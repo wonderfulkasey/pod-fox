d4eb1362e20abbf5f72dc07d76110d5e40d3433f
wonderfulkasey <kaseyarnold15@gmail.com>
2020-09-20 09:51:21 -0400

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
+  root "appointments#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/appointments', to: 'appointments#index'
   get '/appointments/new', to: 'appointments#new'
   get '/appointments/:id', to: 'appointments#show', as: "appointment"
 
+  #resources :appointments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_202733) do
+ActiveRecord::Schema.define(version: 2020_09_17_170526) do
 
   create_table "appointments", force: :cascade do |t|
     t.string "time"
     t.string "date"
     t.integer "doctor_id"
     t.integer "patient_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "doctors", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
