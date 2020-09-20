6661e95bf007450d3369301495c6ceb523ce6f42
wonderfulkasey <kaseyarnold15@gmail.com>
2020-09-20 10:19:13 -0400

    added rest of routes to appointments

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
 
   root "appointments#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/appointments', to: 'appointments#create'
+ 
   get '/appointments', to: 'appointments#index'
   get '/appointments/new', to: 'appointments#new'
   get '/appointments/:id', to: 'appointments#show', as: "appointment"
-  post '/appointments', to: 'appointments#create'
+  
+  get '/appointments/:id/edit', to: 'appointments#edit'
+  get '/appointments/:id', to: 'appointments#update'
+
+  get '/appointments/:id', to: 'appointments#destroy'
   #resources :appointments
 end
