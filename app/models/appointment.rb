457b496624fc8f62df52b49d86debf9c30cb8288
wonderfulkasey <kaseyarnold15@gmail.com>
2020-09-23 16:58:06 -0400

    adding to appointment class

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,6 +3,11 @@ class Appointment < ApplicationRecord
     belongs_to :patient
     belongs_to :doctor
 
+    #setter and getter
+    def doctor_name = (name)
+        self.doctor =
+    end 
+
     validates :time, presence: true 
     validates :date, presence: true
 end
