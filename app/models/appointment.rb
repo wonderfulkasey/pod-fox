e6389f4af776781e2258dcebf482772e71a776c1
wonderfulkasey <kaseyarnold15@gmail.com>
2020-09-23 17:02:00 -0400

    commented out doctor

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,9 +4,9 @@ class Appointment < ApplicationRecord
     belongs_to :doctor
 
     #setter and getter
-    def doctor_name = (name)
-        self.doctor =
-    end 
+    #def doctor_name = (name)
+     #   self.doctor =
+    #end 
 
     validates :time, presence: true 
     validates :date, presence: true
