37bdd71e4461a0e259dbb6f511a0455d0f5192c0
wonderfulkasey <kaseyarnold15@gmail.com>
2020-09-28 15:35:30 -0400

    added a better root index page has docs and apps

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root "appointments#index"
+  root "roots#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post '/appointments', to: 'appointments#create'
  
